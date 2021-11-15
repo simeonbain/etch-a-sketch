eb55fada1d62939993f7a44ab22c5bf74e78c3fb
simeonbain <bainsimeon@gmail.com>
2021-11-16 08:36:16 +1100

    Implement basic etch-a-sketch functionality (standard mode)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,10 +5,11 @@ const maxResolution = 64;
 let resolution = 16;
 let grid = []; 
 
+
 /* Actions to perform on window load/refresh */ 
 function onLoad() { 
   createGrid(); 
-  displayCanvas(); 
+  createCanvas(); 
 }
 
 /* Creates the internal representation of the etch-a-sketch grid */ 
@@ -27,30 +28,43 @@ function createGrid() {
 }
 
 /* Displays the the etch-a-sketch grid on the page */ 
-function displayCanvas() {
+function createCanvas() {
   // clear the existing canvas if any
   while (canvas.firstChild) {
     canvas.removeChild(canvas.firstChild); 
   }
 
   // create the new canvas to be displayed
-  grid.forEach((row) => {
+  grid.forEach((row, rowIndex) => {
     const canvasRow = document.createElement(`div`); 
     canvasRow.classList.add(`canvas-row`);
     canvas.appendChild(canvasRow);
-    row.forEach((column) => {
+
+    row.forEach((column, columnIndex) => {
       const canvasCell = document.createElement(`div`);
       canvasCell.classList.add(`canvas-cell`);
+      canvasCell.setAttribute(`data-row`, rowIndex);
+      canvasCell.setAttribute(`data-column`, columnIndex);
+      canvasCell.classList.add(`inactive`);
       canvasRow.appendChild(canvasCell); 
     });
   });
+}
+
+function onHover(e) {
+  if (e.target.classList.contains(`canvas-cell`)) {
+    const canvasCell = e.target; 
+    grid[canvasCell.dataset.row][canvasCell.dataset.column] = 1; 
+    canvasCell.classList.remove(`inactive`);
+  }
 }
 
 /* Query selectors */ 
 const canvas = document.querySelector(`.canvas`);
 
 /* Event listeners */ 
-window.addEventListener(`load`, onLoad)
+window.addEventListener(`load`, onLoad);
+window.addEventListener(`mouseover`, onHover);
 
 /* Temporary helper functions */ 
 function printGridToConsole() {
